f55687dc674d266f8ad987eac35e14164a0576bc
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2020-05-16 00:58:44 -0300

    Lograda base para generacion random de mapa

---
+++ Bomberman/Assets/Scripts/GameplayController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayController : MonoBehaviour
+{
+    public GameObject floor;
+    public GameObject chunkDestroyable;
+    public GameObject chunkNotDestroyable;
+
+    public int mapWidth;
+    public int mapDepth;
+
+    int chunkSize = 10;
+    int chunkOffset;
+
+    List<GameObject> mapChunks = new List<GameObject>();
+    void Start()
+    {
+        chunkOffset = chunkSize / 2;
+        InstantiateMap();
+    }
+
+    void InstantiateMap()
+    {
+        GameObject go= Instantiate(floor, new Vector3(mapWidth/2.0f*chunkSize,0, mapDepth/2.0f*chunkSize), Quaternion.identity);
+        go.transform.localScale = new Vector3(mapWidth,1, mapDepth);
+
+        for (int i = 0; i < mapWidth; i++)
+        {
+            for (int j = 0; j < mapDepth; j++)
+            {
+                int aux = Random.Range(0, 3);
+
+                if ((i % 2 == 0 && (j+1) % 2 != 0)) aux = 0;
+                else if (i % mapWidth == 0 || (i + 1) % mapWidth == 0 || j % mapDepth == 0 || (j + 1) % mapDepth == 0) aux = 0;
+                else aux = 2;
+
+                switch (aux)
+                {
+                    case 0:
+                        mapChunks.Add(Instantiate(chunkNotDestroyable, new Vector3(i * chunkSize + chunkOffset, chunkOffset, j * chunkSize + chunkOffset), Quaternion.identity));
+                        break;
+                    case 1:
+                        mapChunks.Add(Instantiate(chunkDestroyable, new Vector3(i * chunkSize + chunkOffset, chunkOffset, j * chunkSize + chunkOffset), Quaternion.identity));
+                        break;
+                    case 2:
+                        mapChunks.Add(null);
+                        break;
+                }
+            }
+        }
+    }
+
+}
---
+++ Bomberman/Assets/Scripts/Player3rdPersonControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player3rdPersonControl : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    //SIGUIENTE A HACER, HACER QUE SE MUEVA POR LAS LINEAS Y QUE SI ESTA EN UN CUADRADO QUE PUEDA MOVERSE PARA ABAJO HAGA UN LERP PARA CENTRARSE MIENTRAS SE CENTRA O ALGO ASÍ
+
+    void Update()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(hor, 0, ver) * speed;
+        transform.position += movement * Time.deltaTime;
+
+        Debug.Log("HOR = "+hor);
+        Debug.Log("VER = " + ver);
+    }
+}
